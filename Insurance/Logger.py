539bc1a4eac7e61be4beee3a3ce8444616a98516
Samargithubb <100934815+Samargithubb@users.noreply.github.com>
2023-05-27 15:52:42 +0530

    log file created

--- Insurance/Logger.py
+++ Insurance/Logger.py
@@ -3,6 +3,12 @@ import os
 from datetime import datetime
 from from_root import from_root
 
-LOG_FILE_NAME= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path = os.path.join(from_root(), 'log', LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_NAME,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO
+)
